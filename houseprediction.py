21ccd4196337772e99bda72b4f732b2102eb44e1
Nithish Shanmugam <61447580+Nithish24799@users.noreply.github.com>
2022-12-07 13:40:40 +0530

    u

--- houseprediction.py
+++ houseprediction.py
@@ -519,6 +519,17 @@ price_predict('Indira Nagar',1000,2,2)
 
 
 # In[ ]:
+import streamlit as st
+st.title("Bangalore house price prediction")
+BHK=st.text_input("Please select size(BHK) 1,2,3,4,5")
+sqft=st.text_input("Please select sqftf from 1000 - 5000")
+bath=st.text_input("Please select bath 1,2,3,4,5,")
+location=st.text_input("Please choose area if not specify known please fill 'other'")
+if st.button("Calcuate"):
+    try:
+        st.success(price_predict(location,sqft,bath,BHK))
+    except:
+        st.error("Location is not found Try again")
 
 
 
