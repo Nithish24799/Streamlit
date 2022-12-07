9689325621aeaabaca4b373c96a2d6f10ea14d99
Nithish Shanmugam <61447580+Nithish24799@users.noreply.github.com>
2022-12-07 14:20:44 +0530

    i

--- Nithish.py
+++ Nithish.py
@@ -1,9 +1,19 @@
 
 import streamlit as st
-
-
-# In[12]:
-
+import pandas as pd
+import joblib
+import numpy as np
+X=pd.read_pickle("x.pkl")
+model=joblib.load("model.sav")
+def price_predict(location,sqft,bath,BHK):
+    loc_index=np.where(X.columns==location)[0][0]
+    x=np.zeros(len(X.columns))
+    x[0]=sqft
+    x[1]=bath
+    x[2]=BHK
+    if loc_index >=0:
+        x[loc_index]=1
+    return round((model.predict([x])[0]*100000),0)
 
 st.title("Bangalore house price prediction")
 BHK=st.text_input("Please select size(BHK) 1,2,3,4,5")
